7e0d36d88c1d7f75df50b92e26e0d84cd7788358
Yudz27 <yudz4678@gmail.com>
2025-11-12 18:24:35 +0700

    Add files via upload

---
+++ week9/activity-mandiri/js/script.js
@@ -0,0 +1,27 @@
+function fn_ValForm() {
+    var sMsg = "";
+    var syarat = /^[a-z0-9][a-z0-9_.-]*[a-z0-9]@[a-z0-9][a-z0-9_.-]*[a-z0-9]\.[a-z0-9]{2,4}$/i;
+    var email = document.getElementById("email").value;
+
+    if (document.getElementById("name").value == "") {
+        sMsg += "\n* Anda belum mengisikan nama";
+    }
+
+    if (!email) {
+        sMsg += "\n* Anda belum mengisikan email";
+    }else if(!syarat.test(email)){
+        sMsg += "\n* Format tidak valid";
+    }
+
+    if (document.getElementById("message").value == "") {
+        sMsg += "\n* Anda belum mengisikan pesan";
+    }
+
+    if (sMsg != "") {
+        alert("Peringatan:\n" + sMsg);
+        return false;
+    } else {
+        alert("Form berhasil dikirim!");
+        return true;
+    }
+}
